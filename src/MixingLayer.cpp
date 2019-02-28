ed81af1aa0d22b2bf7d6c62d76fedb39c26c5590
rwestern <rwestern@gmail.com>
2019-02-28 20:07:31 +1100

    Revert "Initial check in"
    This reverts commit e1eb2409c18a4729ca5f456a6eb001a521331ae2.

--- src/MixingLayer.cpp
+++
@@ -1,8 +0,0 @@
-#include "MixingLayer.h"
-
-MixingLayer::MixingLayer(const GfxLayer& top_,
-     const GfxLayer& bottom_,
-     const MixParameters method)
-: top(top_), bottom(bottom_), mixmethod(method)
-{
-}
