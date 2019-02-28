e1eb2409c18a4729ca5f456a6eb001a521331ae2
rwestern <rwestern@gmail.com>
2019-02-28 20:03:50 +1100

    Initial check in

---
+++ include/BinaryLayer.h
@@ -0,0 +1,10 @@
+#ifndef _BINARYLAYER_H_
+#define _BINARYLAYER_H_
+
+#include "GfxLayer.h"
+
+class BinaryLayer : public GfxLayer
+{
+
+};
+#endif
---
+++ include/UnaryLayer.h
@@ -0,0 +1,11 @@
+#ifndef _UNARYLAYER_H_
+#define _UNARYLAYER_H_
+
+#include "GfxLayer.h"
+
+class UnaryLayer : public GfxLayer
+{
+
+};
+
+#endif
